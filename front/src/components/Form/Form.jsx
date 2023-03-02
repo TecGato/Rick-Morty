7b37b6a85e95b83dee7a85c19ea686a30f32693a
Elvis Franco <116116251+TecGato@users.noreply.github.com>
2023-03-02 15:46:18 -0400

    m3 done

--- front/src/components/Form/Form.jsx
+++ front/src/components/Form/Form.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { validate } from './validation';
+import style from './Form.module.css';
 
 export default function Form(props) {
   const [userData, setUserData] = useState({ username: '', password: '' });
@@ -19,7 +20,7 @@ export default function Form(props) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>Username: </label>
+      <label className={style.label}>Username: </label>
       <input
         name='username'
         placeholder='Escriba su Usuario'
@@ -28,8 +29,8 @@ export default function Form(props) {
         onChange={handleChange}
         autoComplete='off'
       />
-      <p>{errors.username}</p>
-      <label>Password: </label>
+      <p className={style.p}>{errors.username}</p>
+      <label className={style.label}>Password: </label>
       <input
         name='password'
         placeholder='Escriba su Password'
@@ -38,10 +39,8 @@ export default function Form(props) {
         onChange={handleChange}
         autoComplete='off'
       />
-      <p>{errors.password}</p>
+      <p className={style.p}>{errors.password}</p>
       <button type='submit'>Entrar</button>
     </form>
   );
 }
-
-
